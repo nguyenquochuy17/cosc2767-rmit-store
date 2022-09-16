97452f13f0193e7ee4b435343922632ab5e7bbea
Huy Pham <huykiengabc9@gmail.com>
2022-09-17 00:44:47 +0700

    fix bug

--- bashscript.sh
+++ bashscript.sh
@@ -1,11 +1,9 @@
 ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/buildAndPushMariadb.yml
 sleep 10
 ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/pullMariadbToCreateContainer.yml
+ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/pullMariadbProd.yml
 sleep 10
 ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/buildAndPushApache.yml
 sleep 10
 ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/pullApacheToCreateContainer.yml
-sleep 10
-ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/pullMariadbProd.yml
-sleep 10
 ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/pullApacheProd.yml
