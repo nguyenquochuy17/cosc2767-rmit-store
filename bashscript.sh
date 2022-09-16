ba10ab9284e88c4186fe683c38b6033bb70bddce
Huy Pham <huykiengabc9@gmail.com>
2022-09-17 00:37:08 +0700

    set up prod env

--- bashscript.sh
+++ bashscript.sh
@@ -5,3 +5,7 @@ sleep 10
 ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/buildAndPushApache.yml
 sleep 10
 ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/pullApacheToCreateContainer.yml
+sleep 10
+ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/pullMariadbProd.yml
+sleep 10
+ansible-playbook /home/ansibleadmin/cosc2767-rmit-store/pullApacheProd.yml
