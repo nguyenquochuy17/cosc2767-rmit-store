f10f6fafe388f5ac762438d8fd48f31cb47efcdf
nguyenquochuy <s3818764@rmit.edu.vn>
2022-09-16 16:21:04 +0700

    udpate

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
-                  "localhost",
+                  "34.201.64.68",
                   "db_admin",
                   "rmit_password",
                   "rmit_store_db"
