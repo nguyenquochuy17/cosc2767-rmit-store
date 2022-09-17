7b0667635f3a5f7fd151a741401947b8f1e7f839
nguyenquochuy <s3818764@rmit.edu.vn>
2022-09-17 23:32:45 +0700

    updated

--- index.php
+++ index.php
@@ -85,13 +85,13 @@
 
         <section class="rmit-store-section row">
             <div class="check_tittle wow fadeInUp" data-wow-delay="0.4s" id="item-list">
-                <h2>Our Store</h2>
+                <h2>Our Store 1</h2>
             </div>
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
                   getenv("DBHOST"),
-                  "db_admin",
+                  "db_admin1",
                   "rmit_password",
                   "rmit_store_db"
               );
